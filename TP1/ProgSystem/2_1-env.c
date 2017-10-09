6345a8b0a3edb38eb8888a3bc0ed7e3892641781
JinhaiZ <zhoujinhaixd@gmail.com>
2017-10-09 10:32:14 +0200

    question 2.2

--- TP1/ProgSystem/2_1-env.c
+++ TP1/ProgSystem/2_1-env.c
@@ -4,10 +4,26 @@
 
 int main(int argc, char **argv) {
 	extern char **environ;
-    
-    for (int i = 0; environ[i] != NULL; i++) {
-        printf("%s\n", environ[i]);
-    }
+	char *env_value;
 
+    if (argc == 1) {
+        for (int i = 0; environ[i] != NULL; i++) {
+            printf("%s\n", environ[i]);
+            i++;
+        }
+    } 
+    else if (argc == 2) {
+        env_value = getenv(argv[1]);
+		if (env_value == NULL){
+			printf("variable %s don't exist\n", argv[1]);
+			return EXIT_FAILURE;
+		} else {
+            printf("%s=%s\n", argv[1], env_value);
+        }
+    } 
+    else {
+        printf("Bad usage : %s [environment_variable]\n", argv[0]);
+		return EXIT_FAILURE;
+    }
     return EXIT_SUCCESS;
 } 
