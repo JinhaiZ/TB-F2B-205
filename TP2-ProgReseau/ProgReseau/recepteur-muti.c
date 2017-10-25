07c6897a5a3292c5bd5ccb6a319289341e0f0b65
JinhaiZ <zhoujinhaixd@gmail.com>
2017-10-25 20:56:42 +0200

    TP2: question 7

--- TP2-ProgReseau/ProgReseau/recepteur-muti.c
+++ TP2-ProgReseau/ProgReseau/recepteur-muti.c
@@ -5,87 +5,103 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 #define BUFSIZE 100000
+#define EXAMPLE_GROUP "224.2.2.10"
 
 int main(int argc, char **argv) {
-  int sfd, s, rsz, r;
-  struct addrinfo hints;
-  struct addrinfo *result, *rp;
-  ssize_t nrecv;
-  char buf[BUFSIZE];
-  struct sockaddr_storage from;
-  socklen_t fromlen;
-  char host[NI_MAXHOST], service[NI_MAXSERV];
+	int sfd, s, rsz, r;
+	struct addrinfo hints;
+	struct addrinfo *result, *rp;
+	ssize_t nrecv;
+	char buf[BUFSIZE];
+	struct sockaddr_storage from;
+	socklen_t fromlen;
+	char host[NI_MAXHOST], service[NI_MAXSERV];
 
-  if (argc != 2) {
-    printf("Usage: %s port\n", argv[0]);
-    exit(EXIT_FAILURE);
-  }
+	if (argc != 2) {
+		printf("Usage: %s port\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
 
- /* Construction de l'adresse locale (pour bind) */
-  memset(&hints, 0, sizeof(struct addrinfo));
-  hints.ai_family = AF_INET6;           /* Force IPv6 */
-  hints.ai_socktype = SOCK_DGRAM;       /* Datagram socket */
-  hints.ai_flags = AI_PASSIVE;          /* Pour l'adresse IP joker */
-  hints.ai_flags |= AI_V4MAPPED|AI_ALL; /* IPv4 remappe en IPv6 */
-  hints.ai_protocol = 0;                /* Any protocol */
+	/* Construction de l'adresse locale (pour bind) */
+	memset(&hints, 0, sizeof(struct addrinfo));
+	hints.ai_family = AF_INET6;           /* Force IPv6 */
+	hints.ai_socktype = SOCK_DGRAM;       /* Datagram socket */
+	hints.ai_flags = AI_PASSIVE;          /* Pour l'adresse IP joker */
+	hints.ai_flags |= AI_V4MAPPED|AI_ALL; /* IPv4 remappe en IPv6 */
+	hints.ai_protocol = 0;                /* Any protocol */
 
-  s = getaddrinfo(NULL, argv[1], &hints, &result);
-  if (s != 0) {
-    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
-    exit(EXIT_FAILURE);
-  }
+	s = getaddrinfo(NULL, argv[1], &hints, &result);
+	if (s != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+		exit(EXIT_FAILURE);
+	}
 
-  /* getaddrinfo() retourne une liste de structures d'adresses.
-     On essaie chaque adresse jusqu'a ce que bind(2) reussisse.
-     Si socket(2) (ou bind(2)) echoue, on (ferme la socket et on)
-     essaie l'adresse suivante. cf man getaddrinfo(3) */
-  for (rp = result; rp != NULL; rp = rp->ai_next) {
+	/* getaddrinfo() retourne une liste de structures d'adresses.
+	   On essaie chaque adresse jusqu'a ce que bind(2) reussisse.
+	   Si socket(2) (ou bind(2)) echoue, on (ferme la socket et on)
+	   essaie l'adresse suivante. cf man getaddrinfo(3) */
+	for (rp = result; rp != NULL; rp = rp->ai_next) {
 
-    /* Creation de la socket */
-    sfd = socket(rp->ai_family, rp->ai_socktype, 0);
-    if (sfd == -1)
-      continue;
+		/* Creation de la socket */
+		sfd = socket(rp->ai_family, rp->ai_socktype, 0);
+		if (sfd == -1)
+			continue;
 
-    /* Association d'un port a la socket */
-    r = bind(sfd, rp->ai_addr, rp->ai_addrlen);
-    if (r == 0)
-      break;            /* Succes */
-    close(sfd);
-  }
+		/* Association d'un port a la socket */
+		r = bind(sfd, rp->ai_addr, rp->ai_addrlen);
+		if (r == 0)
+          break;            /* Succes */
+		close(sfd);
+	}
 
-  if (rp == NULL) {     /* Aucune adresse valide */
-    perror("bind");
-    exit(EXIT_FAILURE);
-  }
-  freeaddrinfo(result); /* Plus besoin */
+	if (rp == NULL) {     /* Aucune adresse valide */
+		perror("bind");
+		exit(EXIT_FAILURE);
+	}
+	freeaddrinfo(result); /* Plus besoin */
 
-  /* Force la taille du buffer de reception de la socket */
-  rsz = 80000;
-  if ( setsockopt(sfd, SOL_SOCKET, SO_RCVBUF, &rsz, sizeof(rsz)) == 0 )
-    printf("SO_RCVBUF apres forcage: %d octets\n", rsz);
-  else
-    perror("setsockopt SO_RCVBUF");
+	struct ip_mreqn multicastArg;
+	multicastArg.imr_multiaddr.s_addr = inet_addr(EXAMPLE_GROUP);
+	multicastArg.imr_address.s_addr = htonl(INADDR_ANY);
+	multicastArg.imr_ifindex = 0;
 
-  /* Boucle de communication */
-  for (;;) {
-    /* Reception donnees */
-    fromlen = sizeof(from);
-    nrecv = recvfrom(sfd, buf, rsz, 0, (struct sockaddr *)&from, &fromlen);
-    if (nrecv == -1) {
-      perror("Erreur en lecture socket\n");
-      exit(EXIT_FAILURE);
-    }
-    printf("Recu %zd octets\n", nrecv);
+	/* connecte au groupe multicast */
+	if (setsockopt(sfd, IPPROTO_IP, IP_ADD_MEMBERSHIP, &multicastArg, 
+				sizeof(multicastArg)) == 0)
+		printf("connecte a adresse multicast \n");
+	else
+		perror("echec de connexion au multicast \n");
 
-    /* Reconnaissance de la machine cliente */
-    s = getnameinfo((struct sockaddr *)&from, fromlen,
-			host, NI_MAXHOST, service, NI_MAXSERV,
-			NI_NUMERICHOST | NI_NUMERICSERV);
-    if (s == 0)
-      printf("Emetteur '%s'  Port '%s'\n", host, service);
-    else
-      printf("Erreur: %s\n", gai_strerror(s));
-  }
+	/* Force la taille du buffer de reception de la socket */
+	rsz = 80000;
+	if ( setsockopt(sfd, SOL_SOCKET, SO_RCVBUF, &rsz, sizeof(rsz)) == 0 )
+		printf("SO_RCVBUF apres forcage: %d octets\n", rsz);
+	else
+		perror("setsockopt SO_RCVBUF");
+
+	/* Boucle de communication */
+	for (;;) {
+		/* Reception donnees */
+		fromlen = sizeof(from);
+		nrecv = recvfrom(sfd, buf, rsz, 0, (struct sockaddr *)&from, &fromlen);
+		if (nrecv == -1) {
+			perror("Erreur en lecture socket\n");
+			exit(EXIT_FAILURE);
+		}
+		printf("Recu %zd octets\n", nrecv);
+		printf("le message est : %s\n",buf);
+
+		/* Reconnaissance de la machine cliente */
+		s = getnameinfo((struct sockaddr *)&from, fromlen,
+				host, NI_MAXHOST, service, NI_MAXSERV,
+				NI_NUMERICHOST | NI_NUMERICSERV);
+		if (s == 0)
+			printf("Emetteur '%s'  Port '%s'\n", host, service);
+		else
+			printf("Erreur: %s\n", gai_strerror(s));
+	}
 }
